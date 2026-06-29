aac5e3a9a976a0bb8a6688596a224801e8a92a8a
RihabMhd <rihabmahdi19@gmail.com>
2026-06-29 16:56:57 +0100

    testing for deployement

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import AuthProvider from './auth/AuthContext.jsx'
 import LanguageProvider from './context/LanguageContext.jsx'
 import { ShopProvider } from './context/ShopContext.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
-
+console.log("API URL:", import.meta.env.VITE_API_URL);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
